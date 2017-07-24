9573a3e022fe825a438c9c27b54948a22908eaf2
ghada <ghadalourimi01@gmail.com>
2017-07-24 11:37:18 +0100

    nav + head

--- js/creative.js
+++ js/creative.js
@@ -62,3 +62,15 @@
     });
 
 })(jQuery); // End of use strict
+$(document).ready(function(){
+    $(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');
+        }
+    );
+});
