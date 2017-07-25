0e8f7d5ace5a68b0152fb6be6a1218af7ceb985e
Ali Hosni <hosni.exe@gmail.com>
2017-07-25 13:49:17 +0100

    db

--- db.php
+++ db.php
@@ -2,9 +2,10 @@
 $servername = "35.160.127.179";
 $username = "alphabet";
 $password = "Kudo5000";
+$db = "alphabet";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password,$db);
 
 // Check connection
 if ($conn->connect_error) {
